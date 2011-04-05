e1fb7ad7d02549974c2c1213bb3ad9be5ff727dc
Martin Lantzsch <martin@linux-doku.de>
2011-04-05 21:18:27 +0200

    Autoloader can now also load classes from currents module directory

--- includes/classAutoLoader.php
+++ includes/classAutoLoader.php
@@ -7,12 +7,15 @@
 
 function load($name) {
     $path = 'includes/classes/'.$name.'.class.php';
+    $moduleClassPath = 'modules/'.utils::current('module').'/classes/'.$name.'.class.php';
     if(file_exists($path))
         require_once $path;
+    elseif(file_exists($moduleClassPath))
+        require_once $moduleClassPath;
     else
-        throw new Exception('classAutoLoader: Class "'.$path.'" not found!');
+        throw new Exception('classAutoLoader: Class "'.$path.'" or "'.$moduleClassPath.'" not found!');
 }
 
-// register autploader
+// register autoloader
 spl_autoload_register(null, false);
 spl_autoload_register('load');

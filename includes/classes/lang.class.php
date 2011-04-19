a005491be5096b78a3ddd4f73ea9bcc18711759f
Christian <secondtruth@protos.(none)>
2011-04-19 18:22:40 +0200

    new language system

--- includes/classes/lang.class.php
+++ includes/classes/lang.class.php
@@ -1,76 +1,85 @@
 <?php
 /**
- * Handling of different languages and caching
+ * Handling of different languages (with caching)
  *
  * @author  Martin Lantzsch <martin@linux-doku.de>
  */
 class lang {
-    private static $lang;
-    private static $strings = array();
 
     /**
-     * Init lang system and load all lang strings (of cache or modules)
-     * @param   string  $lang
+     * The currently selected language
+     * @var     string
+     * @access  private
+     * @static
+     */
+    private static $_lang;
+
+    /**
+     * All registered strings with their translation
+     * @var     array
+     * @access  private
+     * @static
+     */
+    private static $_strings = array();
+
+    /**
+     * Initializes the language system and loads all strings (from database or cache)
+     * @param   string  $lang  The name of the language pack where the strings come from
+     * @access  public
+     * @static
      */
     public static function init($lang) {
-        self::$lang = $lang;
-        // check if cache file exists
-        $langFile = 'includes/cache/lang_'.$lang.'.ini.php';
-        if(file_exists($langFile) && utils::setting('core', 'lang_cache')) {
-            // load lang cache file
-            self::$strings = parse_ini_file($langFile);
+        self::$_lang = $lang;
+        // check if a cache file exists
+        $cacheFile = 'cache/lang_'.$lang.'.ini.php';
+        if (utils::setting('core', 'lang_cache') && file_exists($cacheFile)) {
+            // load all strings from the cache file
+            self::$_strings = parse_ini_file($cacheFile);
         } else {
-            // scan module dirs
-            $moduleDir = scandir('modules/');
-            // remove dots
-            unset($moduleDir[0]);
-            unset($moduleDir[1]);
-            // go through all dirs
-            foreach($moduleDir as $name) {
-                self::add($name);
+            // load all strings from the database
+            $sql = 'SELECT s.string, s.translated FROM {PREFIX}lang_strings s, {PREFIX}lang_packs p'
+                 . 'WHERE p.id = s.pack AND p.code = ?';
+            $result = core::$db->query($sql, array($lang));
+            foreach (core::$db->getAll($result) as $entry) {
+                self::$_strings[$entry['string']] = $entry['translated'];
             }
             // write to cache if enabled
-            if(utils::setting('core', 'lang_cache'))
-                lang::writeCache();
+            if (utils::setting('core', 'lang_cache'))
+                self::writeCache();
         }
     }
 
     /**
-     * Add the lang file of a module
-     * @param   string  $module
+     * Gets the translation of a string
+     * @param   string  $string  The string to translate
+     * @param   array   $vars    Variables ('%var%') to replace as array. The key is the name of the variable (without
+     *                             the percent signs).
+     * @return  string
+     * @access  public
+     * @static
      */
-    public static function add($module) {
-        $langFile = 'modules/'.$module.'/lang/'.self::$lang.'.lang.php';
-        // if file exists add it
-        if(file_exists($langFile))
-            self::addFile($langFile);
+    public static function get($string, $vars = null) {
+        if (isSet(self::$_strings[$string])) {
+            $translated = self::$_strings[$string];
+        } else {
+            $translated = $string;
+        }
+        if (is_array($vars)) {
+            foreach ($vars as $key => $val) {
+                $translated = str_replace('%'.$key.'%', $val, $translated);
+            }
+        }
+        return $translated;
     }
 
     /**
-     * Add a lang file from a path (relative path!)
-     * @param   string  $file
-     */
-    public static function addFile($file) {
-        self::$strings = parse_ini_file($file);
-    }
-
-    /**
-     * write current lang strings to cache
+     * Writes all language strings to a cache file
+     * @return  bool
+     * @access  public
+     * @static
      */
     public static function writeCache() {
-        return utils::writeArrayToIni('includes/cache/lang_'.self::$lang.'.ini.php', self::$strings);
+        return utils::writeArrayToIni('cache/lang_'.self::$_lang.'.ini.php', self::$_strings);
     }
 
-    /**
-     * get a lang string
-     * @param   string  $string
-     * @return  string
-     */
-    public static function get($string) {
-        $var = self::$strings[$string];
-        if($var)
-            return $var;
-        else
-            return $string;
-    }
 }
